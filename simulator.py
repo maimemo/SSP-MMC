a48433480d00b409fd0503268427bb34ab9d1a69
Jarrett Ye <jarrett.ye@outlook.com>
2022-02-08 13:44:10 +0800

    improve output of simulator

--- simulator.py
+++ simulator.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import random
 from model.utils import *
 
-plt.style.use('seaborn-whitegrid')
+plt.style.use('seaborn')
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 plt.rcParams['figure.figsize'] = (6.0, 4.0)
 plt.rcParams['figure.dpi'] = 300
@@ -13,44 +14,22 @@ policy = []
 base = 1.05
 min_index = - 30
 difficulty_offset = 2
-difficulty_limit = 20
-target_halflife = 300
+difficulty_limit = 18
+target_halflife = 360
 
-period_len = 15  # 滚动平均区间
+period_len = 30  # 滚动平均区间
 learn_days = 1000  # 模拟时长
 deck_size = 100000  # 新卡片总量
 
 recall_cost = 3
-forget_cost = 15
+forget_cost = 9
 new_cost = 6
 day_cost_limit = 600
-
-
-def difficulty_distribution(x):
-    if 0 <= x < 0.05:
-        return 10
-    if 0.05 <= x < 0.12:
-        return 9
-    if 0.12 <= x < 0.22:
-        return 8
-    if 0.22 <= x < 0.33:
-        return 7
-    if 0.33 <= x < 0.46:
-        return 6
-    if 0.46 <= x < 0.60:
-        return 5
-    if 0.60 <= x < 0.74:
-        return 4
-    if 0.74 <= x < 0.85:
-        return 3
-    if 0.85 <= x < 0.94:
-        return 2
-    if 0.94 <= x <= 1:
-        return 1
+compare_target = 6000 
 
 
 def scheduler_init():
-    for d in range(1, 21):
+    for d in range(1, difficulty_limit + 1):
         dataset = pd.read_csv(f"./algo/result/ivl-{d}.csv", header=None, index_col=None)
         global policy
         policy.append(dataset.values)
@@ -58,24 +37,27 @@ def scheduler_init():
 
 def scheduler(difficulty, halflife, reps, lapses, method):
     interval = 1
-    if method == 'memorize':
-        interval = sampler(np.log(2) / halflife, 1, learn_days)
-    elif method == 'halflife':
+    if method == 'MEMORIZE':
+        interval = sampler(np.log(2) / halflife, 1, learn_days * 100)
+    elif method == 'HALF-LIFE':
         interval = halflife
-    elif method == 'ssp-mmc':
+    elif method == 'SSP-MMC':
+        halflife = min(halflife, target_halflife)
         index = int(np.log(halflife) / np.log(base) - min_index)
         interval = policy[difficulty - 1][index - 1][1]
-    elif method == 'anki':
+    elif method == 'ANKI':
         interval = max(2.5 - 0.15 * lapses, 1.2) ** reps
-    elif method == 'threshold':
-        interval = - halflife * np.log2(0.8)
+    elif method == 'THRESHOLD':
+        interval = - halflife * np.log2(0.9)
+    elif method == 'RANDOM':
+        interval = random.randint(1, target_halflife)
     return max(1, round(interval))
 
 
 if __name__ == "__main__":
     scheduler_init()
 
-    for method in ['ssp-mmc', 'memorize', 'halflife', 'anki', 'threshold']:
+    for method in ['SSP-MMC', 'MEMORIZE', 'ANKI', 'HALF-LIFE', 'THRESHOLD', 'RANDOM']:
         print("method:", method)
         random.seed(114)
 
@@ -111,7 +93,7 @@ if __name__ == "__main__":
             't_history': 9}
 
         df_memory = pd.DataFrame(np.full(deck_size, np.nan, dtype=dtypes), index=range(deck_size), columns=feature_list)
-        df_memory['difficulty'] = df_memory['difficulty'].map(lambda x: difficulty_distribution(random.random()))
+        df_memory['difficulty'] = df_memory['difficulty'].map(lambda x: random.randint(1, 10))
         df_memory['due_date'] = learn_days
 
         meet_target = 0
@@ -142,7 +124,7 @@ if __name__ == "__main__":
                 if random.random() < p_recall:
                     day_cost += recall_cost
 
-                    df_memory.iat[idx, field_map['r_history']] += '1'
+                    df_memory.iat[idx, field_map['r_history']] += ',1'
 
                     next_halflife = cal_recall_halflife(difficulty, halflife, p_recall)
                     df_memory.iat[idx, field_map['halflife']] = next_halflife
@@ -150,6 +132,7 @@ if __name__ == "__main__":
 
                     if next_halflife >= target_halflife:
                         meet_target += 1
+                        df_memory.iat[idx, field_map['halflife']] = np.inf
                         df_memory.iat[idx, field_map['due_date']] = learn_days
                         continue
 
@@ -159,7 +142,7 @@ if __name__ == "__main__":
                 else:
                     day_cost += forget_cost
 
-                    df_memory.iat[idx, field_map['r_history']] += '0'
+                    df_memory.iat[idx, field_map['r_history']] += ',0'
 
                     next_halflife = cal_forget_halflife(difficulty, halflife, p_recall)
                     df_memory.iat[idx, field_map['halflife']] = next_halflife
@@ -217,54 +200,76 @@ if __name__ == "__main__":
         total_cost = int(sum(cost_per_day))
 
         plt.figure(1)
-        plt.plot(record_per_day, label=f'method={method}')
+        plt.plot(record_per_day, label=f'{method}', linewidth=0.8)
 
         plt.figure(2)
-        plt.plot(meet_target_per_day, label=f'meet_target={meet_target}, method={method}')
+        plt.plot(meet_target_per_day, label=f'{method}', linewidth=0.8)
+        cost_day = np.argmax(meet_target_per_day >= compare_target)
+        if cost_day > 0:
+            print(f'cost day: {cost_day}')
+            plt.plot(cost_day, compare_target, 'k*', linewidth=2)
 
         plt.figure(3)
-        plt.plot(new_item_per_day_average_per_period, label=f'total learned={total_learned}, method={method}')
+        plt.plot(new_item_per_day_average_per_period, label=f'{method}', linewidth=0.8)
 
         plt.figure(4)
-        plt.plot(cost_per_day_average_per_period, label=f'total cost={total_cost}, method={method}')
+        plt.plot(cost_per_day_average_per_period, label=f'{method}', linewidth=0.8)
 
         plt.figure(5)
-        plt.plot(learned_per_day, label=f'total learned={total_learned}, method={method}')
+        plt.plot(learned_per_day, label=f'{method}', linewidth=0.8)
 
         print('acc learn', total_learned)
         print('meet target', meet_target)
 
-        save = df_memory[df_memory['p_recall'] > 0]
+        save = df_memory[df_memory['p_recall'] > 0].copy()
+        save['halflife'] = round(save['halflife'], 4)
+        save['p_recall'] = round(save['p_recall'], 4)
         save.to_csv(f'./simulation_result/{method}.tsv', index=False, sep='\t')
 
     plt.figure(1)
     plt.title(f"day cost limit:{day_cost_limit}-learn days:{learn_days}")
     plt.xlabel("days")
-    plt.ylabel("expectation of memorization")
-    plt.legend()
+    plt.ylabel("SRP")
+    # plt.legend()
     plt.grid(True)
+    with PdfPages('./simulation_result/SRP.pdf') as pdf:
+        # plt.plot(....)
+        pdf.savefig()
     plt.figure(2)
+    plt.plot((0, learn_days), (compare_target, compare_target), color='black', linestyle='dotted', linewidth=0.8)
     plt.title(f"day cost limit:{day_cost_limit}-learn days:{learn_days}")
     plt.xlabel("days")
-    plt.ylabel("num of meet target halflife")
+    plt.ylabel("THR")
     plt.legend()
     plt.grid(True)
+    with PdfPages('./simulation_result/THR.pdf') as pdf:
+        # plt.plot(....)
+        pdf.savefig()
     plt.figure(3)
     plt.title(f"day cost limit:{day_cost_limit}-learn days:{learn_days}")
     plt.xlabel("days")
     plt.ylabel(f"new item per day({period_len} days average)")
     plt.legend()
     plt.grid(True)
+    with PdfPages('./simulation_result/new.pdf') as pdf:
+        # plt.plot(....)
+        pdf.savefig()
     plt.figure(4)
     plt.title(f"day cost limit:{day_cost_limit}-learn days:{learn_days}")
     plt.xlabel("days")
     plt.ylabel(f"cost per day({period_len} days average)")
-    plt.legend()
+    # plt.legend()
     plt.grid(True)
+    with PdfPages('./simulation_result/cost.pdf') as pdf:
+        # plt.plot(....)
+        pdf.savefig()
     plt.figure(5)
     plt.title(f"day cost limit:{day_cost_limit}-learn days:{learn_days}")
     plt.xlabel("days")
-    plt.ylabel(f"num of accumulated learned item({period_len} days average)")
-    plt.legend()
+    plt.ylabel(f"WTL")
+    # plt.legend()
     plt.grid(True)
-    plt.show()
+    # plt.show()
+    with PdfPages('./simulation_result/WTL.pdf') as pdf:
+        # plt.plot(....)
+        pdf.savefig()
